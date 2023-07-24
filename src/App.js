57e2b951443052bb637c3e8f1ed8df4564719cc1
Devendra <mittireddydevendra357@gmail.com>
2023-07-24 10:37:23 +0530

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Product from './components/Product';
+import { createBrowserRouter,createRoutesFromElements,RouterProvider,Route} from "react-router-dom"
+import Dashboard from './components/Dashboard';
+import Cart from './components/Cart';
+import RootLayout from './components/RootLayout';
+function App() {
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+      <Route index element={<Dashboard/>}></Route>
+      <Route path="/cart" element={<Cart/>}></Route>
+    </Route>
+  ))
+
+  return (
+    <div className="App">
+       
+      <RouterProvider router={router} />
+      </div>
+  );
+}
+
+export default App;
